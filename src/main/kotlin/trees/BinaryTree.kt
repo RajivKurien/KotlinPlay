9f1df7c4e0877dd2d43d79c50cd1138c832543f3
Rajiv.Kurien <rajiv.kurien@zuhlke.com>
2023-09-02 09:58:43 +0100

    Add binary traversal

---
+++ src/main/kotlin/trees/BinaryTree.kt
@@ -0,0 +1,36 @@
+package trees
+
+data class Node(val id: Int, val children: List<Node>)
+data class BinaryNode(val id: Int, val left: BinaryNode?, val right: BinaryNode?) {
+    constructor(id: Int) : this(id, null, null)
+}
+
+fun inOrderTraversal(node: BinaryNode?): List<Int> = mutableListOf<Int>().apply {
+    when {
+        node != null -> {
+            addAll(inOrderTraversal(node.left))
+            add(node.id)
+            addAll(inOrderTraversal(node.right))
+        }
+    }
+}
+
+fun preOrderTraversal(node: BinaryNode?): List<Int> = mutableListOf<Int>().apply {
+    when {
+        node != null -> {
+            add(node.id)
+            addAll(preOrderTraversal(node.left))
+            addAll(preOrderTraversal(node.right))
+        }
+    }
+}
+
+fun postOrderTraversal(node: BinaryNode?): List<Int> = mutableListOf<Int>().apply {
+    when {
+        node != null -> {
+            addAll(postOrderTraversal(node.left))
+            addAll(postOrderTraversal(node.right))
+            add(node.id)
+        }
+    }
+}
---
+++ src/test/kotlin/trees/BinaryTreeTest.kt
@@ -0,0 +1,40 @@
+package trees
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class BinaryTreeTest {
+
+    @Test
+    fun `in order traversal`() {
+        val result = inOrderTraversal(binaryTree)
+        assertEquals(listOf(0, 1, 3, 4, 5, 6, 7), result)
+    }
+
+    @Test
+    fun `pre order traversal`() {
+        val result = preOrderTraversal(binaryTree)
+        assertEquals(listOf(1, 0, 6, 4, 3, 5, 7), result)
+    }
+
+    @Test
+    fun `post order traversal`() {
+        val result = postOrderTraversal(binaryTree)
+        assertEquals(listOf(0, 3, 5, 4, 7, 6, 1), result)
+    }
+
+
+    val binaryTree = BinaryNode(
+        id = 1,
+        left = BinaryNode(0),
+        right = BinaryNode(
+            id = 6,
+            left = BinaryNode(
+                id = 4,
+                left = BinaryNode(3),
+                right = BinaryNode(5)
+            ),
+            right = BinaryNode(7)
+        )
+    )
+}
